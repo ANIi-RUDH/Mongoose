f46de89c68ae3400e13a0daf2865b1ad76e7b266
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2023-10-11 23:28:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,50 +3,33 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
 
-
-const personSchema=new mongoose.Schema({
-  name:{
-    type:String,
-  require:true},
-  age: {
-    type:Number,
-    required:1
-  }
-
-})  
-const Person=mongoose.model("Human",personSchema)
-
-
-const individual=new Person({
-  name:"Anirudh Pathania",
-  age:21
-})  
-
-// individual.save()
-// .then((docs)=>{
-//   console.log("New Update for Humans collection",docs)
-// })
-// .catch((err)=>{
-//   console.log("Something is wrong in Humans insertions part",err)
-// })
 const fruitSchema = new mongoose.Schema({
   name: {
     type:String,
-   require:true},
-
-  rating:{
+    require:true},
     
+    rating:{
+      
       type:Number,
       min:1,
       max:10
-  },
-  review:{
+    },
+    review:{
     type:String,
     require:true
   }
+  
 });
 
 const FFruit = mongoose.model("Fruit", fruitSchema);
+
+
+const Papple=new FFruit({
+  name:"Pineapple",
+  rating:10,
+  review:"Great fruit"
+})
+// Papple.save()
 const fruiit = new FFruit({
   name: "Apple",
   rating: 8,
@@ -91,10 +74,57 @@ FFruit.find()
     console.group(item.name )
   })
   console.log("Here is your Orchard",seed);
-  mongoose.connection.close()
+
   })
 // note the words inside the  .then and .catch don't matter what matter is the .then, .catch 
 .catch((happy)=>{
   console.log("Some fruits went missing",happy);
 })
 
+FFruit.updateOne({rating:5},{rating:7})
+.then((docs)=>{
+  console.log("New updation successfull:",docs);
+
+})
+.catch((err)=>{
+  console.log("Some error occured while updating:",err)
+})
+
+
+
+
+const personSchema=new mongoose.Schema({
+  name:{
+    type:String,
+  require:true},
+  age: {
+    type:Number,
+    required:1
+  },
+  favouriteFruit:fruitSchema
+
+});
+const Person=mongoose.model("Human",personSchema)
+
+
+const individual=new Person({
+  name:"Anirudh Pathania",
+  age:21
+})  
+
+// individual.save()
+// .then((docs)=>{
+//   console.log("New Update for Humans collection",docs)
+// })
+// .catch((err)=>{
+//   console.log("Something is wrong in Humans insertions part",err)
+// })
+
+
+const individual2=new Person({
+  name:"Ritta",
+  age:21,
+  favouriteFruit:Papple
+})
+// individual2.save()
+// mongoose.connection.close()
