dfe1eedaac097115799471ca823b1d14f60f7086
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-05-30 11:14:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // Connection URL
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
+mongoose.connect("");
 
 
 const fruitSchema = new mongoose.Schema({
